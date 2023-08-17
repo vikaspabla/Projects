029b45ae0cb805113af16433f401c14ec9ab7551
Vikas Pabla <107341338+vikaspabla@users.noreply.github.com>
2023-08-17 16:36:20 +0530

    Update SQL-MusicStoreDataAnalysis.sql

--- SQL-MusicStoreDataAnalysis.sql
+++ SQL-MusicStoreDataAnalysis.sql
@@ -1,4 +1,5 @@
+Music Store Data Analysis Using SQL
 
-#Question 1 - Who is the senior most employee based on the job title?
+Question 1 - Who is the senior most employee based on the job title?
 
 
