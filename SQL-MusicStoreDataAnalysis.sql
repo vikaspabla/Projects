e3875c132651bc69ef3995d0208c673c2f28ad50
Vikas Pabla <107341338+vikaspabla@users.noreply.github.com>
2023-08-17 16:37:13 +0530

    Update SQL-MusicStoreDataAnalysis.sql

--- SQL-MusicStoreDataAnalysis.sql
+++ SQL-MusicStoreDataAnalysis.sql
@@ -1,5 +1,12 @@
 Music Store Data Analysis Using SQL
 
+We are provided a database of a Music Store and some questions that could be solved with a query in SQL.  
+
 Question 1 - Who is the senior most employee based on the job title?
 
+select *
+from employee
+order by levels desc
+limit 1
 
+Answer - We have the table named employee which holds the information about all the employees. We select the information from employee and to order the table by levels which is respective to employees and their seniority in the company.
